d92dc695475267952fca1d6cdaaa5f86b5d6dedb
Mateo_Liberatore <83372310+MateoLiberatore@users.noreply.github.com>
2025-04-21 17:47:26 -0300

    model comments, clean up

--- src/models/models.py
+++ src/models/models.py
@@ -3,21 +3,21 @@ from datetime import datetime, timezone
 
 db = SQLAlchemy()
 
-# Tabla intermedia: relación muchos a muchos entre órdenes y productos
+# Intermediate table, n to n / order-products
 order_products = db.Table(
     'order_products',
     db.Column('order_id', db.Integer, db.ForeignKey('orders.id'), primary_key=True),
     db.Column('product_id', db.Integer, db.ForeignKey('products.id'), primary_key=True)
 )
 
-# Tabla intermedia: relación muchos a muchos entre órdenes y trabajadores
+# Intermediate table, n to n/ order-workers
 order_workers = db.Table(
     'order_workers',
     db.Column('order_id', db.Integer, db.ForeignKey('orders.id'), primary_key=True),
     db.Column('worker_id', db.Integer, db.ForeignKey('users.id'), primary_key=True)
 )
 
-class User(db.Model):  # se llama User
+class User(db.Model):  
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(25), nullable=False)
@@ -29,15 +29,15 @@ class User(db.Model):  # se llama User
     img = db.Column(db.String(255), nullable=True)
     password = db.Column(db.String(255), nullable=False)
     username = db.Column(db.String(255), nullable=False)
-    category = db.Column(db.String(50), nullable=False)  # Por defecto puede ser "client"
+    category = db.Column(db.String(50), nullable=False) 
 
-    # Un cliente puede tener muchas órdenes
+    # 1 client, n orders
     orders = db.relationship('Order', back_populates='user')
 
-    # Un trabajador puede estar asignado a muchas órdenes (relación muchos a muchos)
+    # 1 worker, n orders
     assigned_orders = db.relationship('Order', secondary=order_workers, backref='workers')
 
-    def __repr__(self):
+    def __repr__(self): #for debbuging purposes 
         return (f"<User id={self.id}, name='{self.name}', surname='{self.surname}', "
                 f"phone='{self.phone}', direction='{self.direction}', email='{self.email}', "
                 f"dni={self.dni}, username='{self.username}', category='{self.category}'>")
@@ -61,8 +61,8 @@ class Order(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc).replace(microsecond=0))
 
-    # Relación muchos a uno: una orden pertenece a un solo usuario (cliente)
+    # 1 order, 1 user (client)
     user = db.relationship('User', back_populates='orders')
 
-    # Relación muchos a muchos con productos
+    # products relations
     products = db.relationship('Product', secondary=order_products, backref='orders')
